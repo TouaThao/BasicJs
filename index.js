aba0c7432768cbd79b834311464e0f00e2f9082b
Toua Thao <touathao09@gmail.com>
2019-02-16 14:02:38 -0600

    back to basic

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 console.log('Hello World')
+////////////////////////////
+//Rule for naming variable
+//Cannot be a resever keywords
+//They should be meaningful
+//They cannot start with a number
+//They cannot contain a space or hyphen
+//They are case sensitive
+let name = 'Toua';
+console.log(name);
+let firstName = 'Toua';
+let lastName = 'Thao';
+////////////////////////////
+//Const
+// We use const to store data that we don't want to change
+const intrestRate = .07;
+console.log(intrestRate);
+////////////////////////////
+//We have two type. One is primitives/value type.The other is reference type
+//Primitive are string, number, boolean,undefine and null
+let sky ='blue'; // string
+let age = 1000; //number
+let isBlue = false; //boolean
+let grayGoose; //undefine
+let selectedColor = null;// null
+////////////////////////////
+//Reference type
+//They're object,function and array
